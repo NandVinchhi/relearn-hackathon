c5c83b4ebadd6a0fbd0c6172e59f40a52b1461a6
Nand Vinchhi <nand@getairchat.com>
2023-11-11 10:53:00 -0500

    initial commit with app UI

---
+++ backend/insert.py
@@ -0,0 +1,75 @@
+units = [
+    {
+        "unit_id": 1,
+        "topic_id": 1,
+        "unit_name": "Chemistry of Life",
+        "unit_number": 1
+    },
+    {
+        "unit_id": 2,
+        "topic_id": 1,
+        "unit_name": "Cell Structure and function",
+        "unit_number": 2
+    },
+    {
+        "unit_id": 3,
+        "topic_id": 1,
+        "unit_name": "Cellular Energetics",
+        "unit_number": 3
+    },
+    {
+        "unit_id": 4,
+        "topic_id": 2,
+        "unit_name": "Atomic structure and properties",
+        "unit_number": 1
+    },
+    {
+        "unit_id": 5,
+        "topic_id": 2,
+        "unit_name": "Molecular and ionic compound structures and properties",
+        "unit_number": 2
+    },
+    {
+        "unit_id": 6,
+        "topic_id": 2,
+        "unit_name": "Intermolecular forces and properties",
+        "unit_number": 3
+    },
+    {
+        "unit_id": 7,
+        "topic_id": 3,
+        "unit_name": "The living world: ecosystems and biodiversity",
+        "unit_number": 1
+    },
+    {
+        "unit_id": 8,
+        "topic_id": 4,
+        "unit_name": "Period 1: 1491-1607",
+        "unit_number": 1
+    },
+    {
+        "unit_id": 9,
+        "topic_id": 4,
+        "unit_name": "Period 2: 1607-1754",
+        "unit_number": 2
+    },
+    {
+        "unit_id": 10,
+        "topic_id": 4,
+        "unit_name": "Period 3: 1754-1800",
+        "unit_number": 3
+    },
+    {
+        "unit_id": 11,
+        "topic_id": 5,
+        "unit_name": "Basic economics concepts",
+        "unit_number": 1
+    },
+    {
+        "unit_id": 12,
+        "topic_id": 6,
+        "unit_name": "Basic Economic Concepts",
+        "unit_number": 1
+    }
+]
+
