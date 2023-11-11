ee63733c5a92f69809bcd356bcddd6e5b7e41836
Nand Vinchhi <nand@getairchat.com>
2023-11-11 11:24:24 -0500

    Never see you again, UserInterfaceState

---
+++ Relearn/Relearn/LoadingView.swift
@@ -0,0 +1,18 @@
+//
+//  LoadingView.swift
+//  Relearn
+//
+//  Created by Nand Vinchhi on 11/11/23.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoadingView()
+}
